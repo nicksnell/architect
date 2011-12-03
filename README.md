fdf03d524deeffa086f48ade1ff86fc5affb7032
Nick Snell <nick@orpo.co.uk>
2011-12-03 11:28:14 +0000

    Work towards automatic github deployment.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Architect - Web application builder toolkit
 ===========================================
+
+A collection of tools for building websites/applications using the Fabric tool.
+
+
+### Notes
+
+- py-github is included in the source as it's not available via py-pi and is needed for the github module. In future py-github2 will be used, however at this time it doesn't support deployment keys.
---
+++ architect/github.py
@@ -0,0 +1,20 @@
+"""Management commands for dealing with github"""
+
+import os.path
+
+from fabric.api import *
+from fabric.colors import green
+from fabric.decorators import task
+
+import github
+
+@task
+def add_deploy_key(key):
+	"""Add a deployment key to the repository"""
+	
+	require('github_user', provided_by=('development', 'staging', 'production'))
+	require('github_api_key', provided_by=('development', 'staging', 'production'))
+	require('github_repo_name', provided_by=('development', 'staging', 'production'))
+	
+	client = github.GitHub(env.github_user, env.github_api_key)
+	
